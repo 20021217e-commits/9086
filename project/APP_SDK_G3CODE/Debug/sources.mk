e2a554dd4411989eb73937a391ee0a4b9b076386
20021217e-commits <20021217e@gmail.com>
2026-07-02 15:45:50 +0800

    Bulk upload from ZIP [batch 46]

---
+++ project/APP_SDK_G3CODE/Debug/sources.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+O_SRCS := 
+C_SRCS := 
+S_SRCS := 
+S_UPPER_SRCS := 
+OBJ_SRCS := 
+ASM_SRCS := 
+SAG_SRCS += \
+../nds32-ae210p.sag
+OBJS := 
+C_DEPS := 
+GCOV_OUT := 
+LDSAG_OUT := 
+SYMBOL_OUTPUTS := 
+OBJDUMP_OUTPUTS := 
+READELF_OUTPUTS := 
+GPROF_OUT := 
+SIZE_OUTPUTS := 
+EXECUTABLES := 
+S_UPPER_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+FDL/src \
+HAL_Lib/src \
+Sata/src/ata_cmd \
+Sata/src/driver \
+Sata/src/sata_protocol \
+Sata/src/scsi \
+Sata/src/task_manage \
+Sata/src/task_process \
+SysCfg/src/platform \
+SysCfg/src/startup \
+src \
+
