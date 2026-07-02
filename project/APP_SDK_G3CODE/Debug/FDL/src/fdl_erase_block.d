37a0db1eaec12c2c333cb5c4a865bdc9187a84b2
20021217e-commits <20021217e@gmail.com>
2026-07-02 15:43:08 +0800

    Bulk upload from ZIP [batch 35]

---
+++ project/APP_SDK_G3CODE/Debug/FDL/src/fdl_erase_block.d
@@ -0,0 +1,119 @@
+FDL/src/fdl_erase_block.d FDL/src/fdl_erase_block.o: \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/src/fdl_erase_block.c \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_erase_block.h \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_task_op.h \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver.h \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver_struct.h \
+ /cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/data_type.h \
+ /cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/hal.h \
+ /cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/hal_config.h \
+ /cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/data_type.h \
+ /cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/flash_op_cmd.h \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver_config.h \
+ /cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/inc/hal_cq.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/startup/ae210p.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regCPM.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regADMCOM.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA1.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA2.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regFC.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regMCU.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPadctrl.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSPI.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regUART.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSC.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPMCP.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regLDPC.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/system_sg9081.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/mcu.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uitility.h \
+ /cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/inc/Hal_ldpc.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/Debug.h \
+ /cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/hal_pmcp.h \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver_config.h \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver_struct.h \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_debug.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/debug.h \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_debug.h \
+ /cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_common.h
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_erase_block.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_task_op.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver_struct.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/data_type.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/hal.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/hal_config.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/data_type.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/flash_op_cmd.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver_config.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/inc/hal_cq.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/startup/ae210p.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regCPM.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regADMCOM.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA1.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA2.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regFC.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regMCU.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPadctrl.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSPI.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regUART.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSC.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPMCP.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regLDPC.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/system_sg9081.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/mcu.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uitility.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/inc/Hal_ldpc.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/Debug.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/APP_HAL_Lib/HAL_Lib/interface/hal_pmcp.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver_config.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/Interface/flash_driver_struct.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_debug.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/debug.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_debug.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/FlashDriver/FDL/inc/fdl_common.h:
