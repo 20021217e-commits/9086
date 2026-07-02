a2e47a0e9310a97a85a112291ee21e0b937ec1e6
20021217e-commits <20021217e@gmail.com>
2026-07-02 15:44:47 +0800

    Bulk upload from ZIP [batch 42]

---
+++ project/APP_SDK_G3CODE/Debug/Sata/src/ata_cmd/ata_cmd_list.d
@@ -0,0 +1,104 @@
+Sata/src/ata_cmd/ata_cmd_list.d Sata/src/ata_cmd/ata_cmd_list.o: \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/src/ata_cmd/ata_cmd_list.c \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/debug.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/startup/ae210p.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regCPM.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regADMCOM.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA1.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA2.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regFC.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regMCU.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPadctrl.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSPI.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regUART.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSC.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPMCP.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regLDPC.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/system_sg9081.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/mcu.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uitility.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_cmd_list.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_typedef.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_identify.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_general.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_fpdma.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_cmd_list.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_gpl.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_smart.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_dco.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_smart.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_power_mg.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_security.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_sanitize.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_hpa.h
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/debug.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/startup/ae210p.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regCPM.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regADMCOM.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA1.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA2.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regFC.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regMCU.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPadctrl.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSPI.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regUART.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSC.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPMCP.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regLDPC.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/system_sg9081.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/mcu.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uitility.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_cmd_list.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_typedef.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_identify.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_general.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_fpdma.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_cmd_list.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_gpl.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_smart.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_dco.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_smart.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_power_mg.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_security.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_sanitize.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_hpa.h:
---
+++ project/APP_SDK_G3CODE/Debug/Sata/src/ata_cmd/ata_gpl.d
@@ -0,0 +1,119 @@
+Sata/src/ata_cmd/ata_gpl.d Sata/src/ata_cmd/ata_gpl.o: \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/src/ata_cmd/ata_gpl.c \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_gpl.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_typedef.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_smart.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/debug.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/startup/ae210p.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regCPM.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regADMCOM.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA1.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA2.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regFC.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regMCU.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPadctrl.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSPI.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regUART.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSC.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPMCP.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regLDPC.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/system_sg9081.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/mcu.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uitility.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_general.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_identify.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_pio_in.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_typedef.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_pio_out.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_dma_in.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_dma_out.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_misc.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/driver/dsata.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/task_manage/task_manage.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/task_process/task_process.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uitility.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_smart.h \
+ /cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_dco.h \
+ /cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uart.h
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_gpl.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_typedef.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_smart.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/debug.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/startup/ae210p.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regCPM.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regADMCOM.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA1.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regDMA2.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regFC.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regMCU.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPadctrl.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSPI.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regUART.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regSC.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regPMCP.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/regLDPC.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/system_sg9081.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/mcu.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uitility.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_general.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_identify.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/common.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_pio_in.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_typedef.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_pio_out.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_dma_in.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_dma_out.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/sata_protocol/sata_misc.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/driver/dsata.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/task_manage/task_manage.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/sg9081.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/task_process/task_process.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uitility.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_smart.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/Sata/inc/ata_cmd/ata_dco.h:
+
+/cygdrive/D/development/Automation/9086BB/9086/SysCfg/inc/platform/uart.h:
